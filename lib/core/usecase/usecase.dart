9404d9dff1575e879f309e39bc240aca7ce3e0f7
Johnson <johnsonchong0123@gmail.com>
2026-02-13 14:26:43 +0800

    refactor: reorganize domain layer structure
    - Move User entity from core to features/domain/entity
    - Update usecase base class
    - Improve domain layer organization

--- lib/core/usecase/usecase.dart
+++ lib/core/usecase/usecase.dart
@@ -2,7 +2,8 @@ import 'package:equatable/equatable.dart';
 import 'package:fpdart/fpdart.dart';
 
 import '../errors/failure.dart';
-
+/// Base use case interface.
+/// Returns Left(Failure) on error, Right(ReturnType) on success.
 abstract interface class UseCase<ReturnType, Params> {
   Future<Either<Failure, ReturnType>> call(Params params);
 }
